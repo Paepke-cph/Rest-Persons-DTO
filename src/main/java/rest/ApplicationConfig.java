3facc2273333eb02687b66278ba43ba8cd85719f
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2020-03-07 07:54:18 +0100

    Test

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -11,19 +11,4 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(GenericExceptionMapper.class);
-        resources.add(MissingInputMapper.class);
-        resources.add(PersonNotFoundMapper.class);
-        resources.add(PersonResource.class);
-    }
-    
 }
